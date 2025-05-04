dfb755da71a81db736c0ef18022818120266223f
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-05-04 09:22:13 +0530

    ADD: DB configurations

---
+++ backend/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import{DB_NAME} from "./constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("MongoDB connected", connectionInstance.connection.host, connectionInstance.connection.name);
+    } catch (error) {
+        console.log("MongoDB connection error", error);
+        
+    }
+}
+
+export default connectDB
