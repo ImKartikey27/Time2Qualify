356c9e40d7f1e0f4225b0e6593651a7c2618eca6
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-05-04 19:10:55 +0530

    ADD: Admin model and fucntions

---
+++ backend/src/models/admin.models.js
@@ -0,0 +1,65 @@
+import mongoose, { Schema } from 'mongoose';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const adminSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user',
+    },
+    refreshToken: {
+        type: String,
+    },
+},{
+    timestamps: true,
+});
+
+adminSchema.pre("save", async function(next) {
+    if(!this.isModified("password")){
+        return next()
+    }
+    const salt = await bcryptjs.genSalt(10);
+    this.password = await bcryptjs.hash(this.password, salt);
+    next();
+})
+
+adminSchema.methods.isPasswordCorrect = async function(password){
+    return await bcryptjs.compare(password, this.password)
+}
+
+adminSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        id: this._id,
+        username: this.username,
+        email: this.email,
+    }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: ACCESS_TOKEN_EXPIRY,
+    })
+}
+
+const Admin = mongoose.model('Admin', adminSchema);
+export default Admin;
